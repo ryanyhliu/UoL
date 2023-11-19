05ac983084fade9528c742d806af30ffa48f23e5
ryanyhliu <isryanyhliu@foxmail.com>
2023-11-19 13:43:14 +0000

    修改引用的代码

--- COMP528/Assignments/Assignment01/ompcInsertion_discard.c
+++ COMP528/Assignments/Assignment01/ompcInsertion_discard.c
@@ -6,100 +6,100 @@
 #include <omp.h>
 #include <time.h>
 
-/*This code is for reading and writing to files for the 2023-24 COMP528 CA1*/
+// /*This code is for reading and writing to files for the 2023-24 COMP528 CA1*/
 
-/*Use the functions in this file to read from the input file, and write to the output file*/
+// /*Use the functions in this file to read from the input file, and write to the output file*/
 
-/*You should use this file when compiling your code*/
+// /*You should use this file when compiling your code*/
 
-/*Declare these functions at the top of each 'main' file*/
+// /*Declare these functions at the top of each 'main' file*/
 
-/*If there are any issues with this code, please contact: h.j.forbes@liverpool.ac.uk*/
+// /*If there are any issues with this code, please contact: h.j.forbes@liverpool.ac.uk*/
 
-int readNumOfCoords(char *inputFileName);
-double **readCoords(char *filename, int numOfCoords);
+// int readNumOfCoords(char *inputFileName);
+// double **readCoords(char *filename, int numOfCoords);
 
-/*Gets the number of the coordinates in the file. Returns as a single integer*/
-int readNumOfCoords(char *filename)
-{
-	FILE *file = fopen(filename, "r");
-	int numOfCoords = 0;
+// /*Gets the number of the coordinates in the file. Returns as a single integer*/
+// int readNumOfCoords(char *filename)
+// {
+// 	FILE *file = fopen(filename, "r");
+// 	int numOfCoords = 0;
 
-	if (file == NULL)
-	{
-		return -1;
-	}
+// 	if (file == NULL)
+// 	{
+// 		return -1;
+// 	}
 
-	char line[100];
+// 	char line[100];
 
-	while (fgets(line, sizeof(line), file) != NULL)
-	{
-		numOfCoords++;
-	}
+// 	while (fgets(line, sizeof(line), file) != NULL)
+// 	{
+// 		numOfCoords++;
+// 	}
 
-	return numOfCoords;
-}
+// 	return numOfCoords;
+// }
 
-/*Gets the data from the file. Returns as an array of doubles, Ignores the first integer*/
-double **readCoords(char *filename, int numOfCoords)
-{
-	FILE *file = fopen(filename, "r");
-	int i;
+// /*Gets the data from the file. Returns as an array of doubles, Ignores the first integer*/
+// double **readCoords(char *filename, int numOfCoords)
+// {
+// 	FILE *file = fopen(filename, "r");
+// 	int i;
 
-	char line[100];
+// 	char line[100];
 
-	if (file == NULL)
-	{
-		printf("Unable to open file: %s", filename);
-		return NULL;
-	}
+// 	if (file == NULL)
+// 	{
+// 		printf("Unable to open file: %s", filename);
+// 		return NULL;
+// 	}
 
-	double **coords = (double **)malloc(numOfCoords * sizeof(double *));
+// 	double **coords = (double **)malloc(numOfCoords * sizeof(double *));
 
-	for (i = 0; i < numOfCoords; i++)
-	{
-		coords[i] = (double *)malloc(2 * sizeof(int));
-		if (coords[i] == NULL)
-		{
-			perror("Memory Allocation Failed");
-		}
-	}
+// 	for (i = 0; i < numOfCoords; i++)
+// 	{
+// 		coords[i] = (double *)malloc(2 * sizeof(int));
+// 		if (coords[i] == NULL)
+// 		{
+// 			perror("Memory Allocation Failed");
+// 		}
+// 	}
 
-	int lineNum = 0;
-	while (fgets(line, sizeof(line), file) != NULL)
-	{
-		double x, y;
-		if (sscanf(line, "%lf,%lf", &x, &y) == 2)
-		{
-			coords[lineNum][0] = x;
-			coords[lineNum][1] = y;
-			lineNum++;
-		}
-	}
+// 	int lineNum = 0;
+// 	while (fgets(line, sizeof(line), file) != NULL)
+// 	{
+// 		double x, y;
+// 		if (sscanf(line, "%lf,%lf", &x, &y) == 2)
+// 		{
+// 			coords[lineNum][0] = x;
+// 			coords[lineNum][1] = y;
+// 			lineNum++;
+// 		}
+// 	}
 
-	return coords;
-}
+// 	return coords;
+// }
 
-void *writeTourToFile(int *tour, int tourLength, char *filename)
-{
+// void *writeTourToFile(int *tour, int tourLength, char *filename)
+// {
 
-	FILE *file = fopen(filename, "w");
-	int i;
+// 	FILE *file = fopen(filename, "w");
+// 	int i;
 
-	if (file == NULL)
-	{
-		printf("Unable to open file: %s", filename);
-		return NULL;
-	}
+// 	if (file == NULL)
+// 	{
+// 		printf("Unable to open file: %s", filename);
+// 		return NULL;
+// 	}
 
-	fprintf(file, "%d \n", tourLength);
+// 	fprintf(file, "%d \n", tourLength);
 
-	printf("Writing output data\n");
-	for (i = 0; i < tourLength; i++)
-	{
-		fprintf(file, "%d ", tour[i]);
-	}
-}
+// 	printf("Writing output data\n");
+// 	for (i = 0; i < tourLength; i++)
+// 	{
+// 		fprintf(file, "%d ", tour[i]);
+// 	}
+// }
 
 ////////////////// YH's Code Begin
 
