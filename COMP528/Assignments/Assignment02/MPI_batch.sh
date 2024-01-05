cd2e770500aa8e4fdfa0e817be2aef11c07e9978
Ryan Liu <isryanyhliu@foxmail.com>
2024-01-05 22:50:00 +0000

    rm compile from mpi batch

--- COMP528/Assignments/Assignment02/MPI_batch.sh
+++ COMP528/Assignments/Assignment02/MPI_batch.sh
@@ -1,6 +1,6 @@
 #!/bin/bash -l
 
-# SLURM Batch Script for MPI Program
+# SLURM Batch Script for running an MPI program
 
 #SBATCH -D ./
 #SBATCH --export=ALL
@@ -8,32 +8,20 @@
 #SBATCH -t 5
 #SBATCH -c 32
 
-# Load modules
+# Load necessary modules
 module load compilers/intel/2019u5
 module load mpi/intel-mpi/2019u5/bin
 
-# Read arguments
-SRC=$1
+# Assign arguments to variables
+EXE=$1
 COORD_FILE=$2
 OUT_FILE_1=$3
 OUT_FILE_2=$4
 OUT_FILE_3=$5
 
-# Set the executable name from the source code name (remove ".c" and append ".exe")
-EXE=${SRC%%.c}.exe
-
-# Delete the existing executable, if it exists
-rm -f ${EXE}
-
-echo "Compiling $SRC to $EXE"
-# ADD COMPILER INSTRUCTION HERE.
-mpiicc -no-multibyte-chars $SRC -o $EXE
-echo "Compilation Complete"
-echo -------------------------
-
-# Check if the executable was successfully created
+# Check if the executable exists
 if [ ! -f "$EXE" ]; then
-    echo "Compilation failed, executable $EXE not found!"
+    echo "Executable $EXE not found!"
     exit 1
 fi
 
@@ -43,5 +31,5 @@ echo -------------------------
 # SLURM_NTASKS is given by the -n flag when using sbatch.
 procs=${SLURM_NTASKS:-1}
 
-# RUN THE PROGRAM HERE USING $procs WITH THE -np FLAG.
+# Run the MPI program
 mpirun -np $procs ./$EXE $COORD_FILE $OUT_FILE_1 $OUT_FILE_2 $OUT_FILE_3
