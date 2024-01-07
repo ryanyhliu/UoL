dd868c0cc8eff61ca6f62395eea826a0a77cc604
Ryan Liu <isryanyhliu@foxmail.com>
2024-01-07 14:06:32 +0000

    1

--- COMP528/Assignments/Assignment02/ompfInsertion.c
+++ COMP528/Assignments/Assignment02/ompfInsertion.c
@@ -16,107 +16,30 @@ double getDistance_FarthestInsertion(double **dMatrix, int numOfCoords) {
     int *tour = malloc((numOfCoords + 1) * sizeof(int));
     bool *visited = calloc(numOfCoords, sizeof(bool));
     double totalDistance = 0.0;
+    int numVisited = 1;
+    int tourLength = 2;  // Starting with the first point
     
-    // Initialize tour to -1 indicating unvisited
     for (int i = 0; i <= numOfCoords; ++i) {
         tour[i] = -1;
     }
 
-    // Start tour at the first node
-    int startNode = 0; // Assuming the first node as the start node
-    tour[0] = startNode;
-    tour[numOfCoords] = startNode; // End at the start node to make a loop
-    visited[startNode] = true;
+    tour[0] = 0;  // Assuming 0 as the start node
+    tour[1] = 0;  // Loop back to the start
+    visited[0] = true;
 
-    int numVisited = 1;
-    int tourLength = 1; // Current length of the tour
+    int numThreads = omp_get_max_threads();
+    double *threadMaxCosts = malloc(numThreads * sizeof(double));
+    int *threadFarthestNodes = malloc(numThreads * sizeof(int));
 
     while (numVisited < numOfCoords) {
-        double maxDistance = -1.0;
-        int farthestNode = -1;
+        double globalMaxDistance = -1.0;
+        int globalFarthestNode = -1;
 
-        // Find the farthest unvisited node
-        #pragma omp parallel for
-        for (int i = 0; i < numOfCoords; ++i) {
-            if (!visited[i]) {
-                double distance = 0.0;
-                for (int j = 0; j < tourLength; ++j) {
-                    distance += dMatrix[tour[j]][i];
-                }
-
-                #pragma omp critical
-                {
-                    if (distance > maxDistance) {
-                        maxDistance = distance;
-                        farthestNode = i;
-                    }
-                }
-            }
-        }
-
-        // Find the position where the farthest node will be inserted
-        double minInsertionCost = __DBL_MAX__;
-        int insertPosition = -1;
-        for (int i = 0; i < tourLength; ++i) {
-            int nextIndex = (i + 1) % tourLength;
-            double cost = dMatrix[tour[i]][farthestNode] + dMatrix[farthestNode][tour[nextIndex]] - dMatrix[tour[i]][tour[nextIndex]];
-
-            if (cost < minInsertionCost) {
-                minInsertionCost = cost;
-                insertPosition = i + 1;
-            }
-        }
-
-        // Insert the farthest node at the found position
-        for (int i = tourLength; i >= insertPosition; --i) {
-            tour[i + 1] = tour[i];
-        }
-        tour[insertPosition] = farthestNode;
-        visited[farthestNode] = true;
-        totalDistance += minInsertionCost;
-        numVisited++;
-        tourLength++;
-    }
-
-    // Connect the last node to the start node
-    totalDistance += dMatrix[tour[tourLength - 1]][tour[0]];
-
-    // Clean up
-    free(tour);
-    free(visited);
-
-    // Return the total distance of the tour
-    return totalDistance;
-}
-
-
-
-
-int *getTour_FarthestInsertion(double **dMatrix, int numOfCoords) {
-    int *tour = malloc((1 + numOfCoords) * sizeof(int));
-    bool *visited = calloc(numOfCoords, sizeof(bool));
-    int tourLength = 1;  // Starting with the first point
-    
-    // Initialize tour with -1 indicating unvisited
-    for (int i = 0; i <= numOfCoords; ++i) {
-        tour[i] = -1;
-    }
-
-    // Start the tour at the first point (assuming 0 as the start)
-    tour[0] = 0;
-    visited[0] = true;
-    
-    // Continue until all nodes are visited
-    while (tourLength < numOfCoords) {
-        double maxDistance = -1.0;
-        int farthestNode = -1;
-
-        // Parallel section to find the farthest unvisited node
         #pragma omp parallel
         {
             int threadID = omp_get_thread_num();
-            double threadMaxDistance = -1.0;
-            int threadFarthestNode = -1;
+            double localMaxDistance = -1.0;
+            int localFarthestNode = -1;
 
             #pragma omp for nowait
             for (int i = 0; i < numOfCoords; ++i) {
@@ -125,33 +48,119 @@ int *getTour_FarthestInsertion(double **dMatrix, int numOfCoords) {
                     for (int j = 0; j < tourLength; ++j) {
                         nodeDistance += dMatrix[tour[j]][i];
                     }
-
-                    #pragma omp critical
-                    {
-                        if (nodeDistance > threadMaxDistance) {
-                            threadMaxDistance = nodeDistance;
-                            threadFarthestNode = i;
-                        }
+                    if (nodeDistance > localMaxDistance) {
+                        localMaxDistance = nodeDistance;
+                        localFarthestNode = i;
                     }
                 }
             }
 
-            // Critical section to update the global farthest node
             #pragma omp critical
             {
-                if (threadMaxDistance > maxDistance) {
-                    maxDistance = threadMaxDistance;
-                    farthestNode = threadFarthestNode;
+                if (localMaxDistance > globalMaxDistance) {
+                    globalMaxDistance = localMaxDistance;
+                    globalFarthestNode = localFarthestNode;
                 }
             }
         }
 
-        // Find the best insertion position for the farthest node
-        double minInsertionCost = __DBL_MAX__;
+        double minInsertionCost = DBL_MAX;
         int bestInsertPos = -1;
-        for (int i = 0; i < tourLength; ++i) {
-            int nextIndex = (i + 1) % tourLength;
-            double insertionCost = dMatrix[tour[i]][farthestNode] + dMatrix[farthestNode][tour[nextIndex]] - dMatrix[tour[i]][tour[nextIndex]];
+        for (int i = 0; i < tourLength - 1; ++i) {
+            double cost = dMatrix[tour[i]][globalFarthestNode] + dMatrix[globalFarthestNode][tour[i + 1]] - dMatrix[tour[i]][tour[i + 1]];
+            if (cost < minInsertionCost) {
+                minInsertionCost = cost;
+                bestInsertPos = i + 1;
+            }
+        }
+
+        for (int i = tourLength; i >= bestInsertPos; --i) {
+            tour[i] = tour[i - 1];
+        }
+        tour[bestInsertPos] = globalFarthestNode;
+        visited[globalFarthestNode] = true;
+        totalDistance += minInsertionCost;
+        numVisited++;
+        tourLength++;
+    }
+
+    // Add distance back to the start node
+    totalDistance += dMatrix[tour[tourLength - 1]][tour[0]];
+
+    free(tour);
+    free(visited);
+    free(threadMaxCosts);
+    free(threadFarthestNodes);
+
+    return totalDistance;
+}
+
+
+
+
+
+int *getTour_FarthestInsertion(double **dMatrix, int numOfCoords) {
+    int *tour = malloc((numOfCoords + 1) * sizeof(int));
+    bool *visited = calloc(numOfCoords, sizeof(bool));
+    int numVisited = 1;
+    int tourLength = 2;  // Starting with the first point
+    
+    // Initialize tour with -1 indicating unvisited
+    for (int i = 0; i < numOfCoords; ++i) {
+        tour[i] = -1;
+    }
+
+    // Start the tour at the first point (assuming 0 as the start)
+    tour[0] = 0;
+    tour[numOfCoords] = 0; // End at the start to make a loop
+    visited[0] = true;
+    
+    // Get the number of threads
+    int numThreads = omp_get_max_threads();
+    
+    // Allocate memory for local data per thread to prevent false sharing
+    double *threadMaxCosts = malloc(numThreads * sizeof(double));
+    int *threadFarthestNodes = malloc(numThreads * sizeof(int));
+    
+    while (numVisited < numOfCoords) {
+        // Find the farthest unvisited node
+        #pragma omp parallel
+        {
+            int threadID = omp_get_thread_num();
+            double maxDistance = -DBL_MAX;
+            int farthestNode = -1;
+            #pragma omp for nowait
+            for (int i = 0; i < numOfCoords; ++i) {
+                if (!visited[i]) {
+                    double nodeDistance = 0.0;
+                    for (int j = 0; j < tourLength; ++j) {
+                        nodeDistance += dMatrix[tour[j]][i];
+                    }
+                    if (nodeDistance > maxDistance) {
+                        maxDistance = nodeDistance;
+                        farthestNode = i;
+                    }
+                }
+            }
+            threadMaxCosts[threadID] = maxDistance;
+            threadFarthestNodes[threadID] = farthestNode;
+        }
+
+        // Reduce the results to find the overall farthest node
+        double globalMaxDistance = -DBL_MAX;
+        int globalFarthestNode = -1;
+        for (int i = 0; i < numThreads; ++i) {
+            if (threadMaxCosts[i] > globalMaxDistance) {
+                globalMaxDistance = threadMaxCosts[i];
+                globalFarthestNode = threadFarthestNodes[i];
+            }
+        }
+        
+        // Find the best position to insert the farthest node
+        double minInsertionCost = DBL_MAX;
+        int bestInsertPos = -1;
+        for (int i = 0; i < tourLength - 1; ++i) {
+            double insertionCost = dMatrix[tour[i]][globalFarthestNode] + dMatrix[globalFarthestNode][tour[i + 1]] - dMatrix[tour[i]][tour[i + 1]];
             if (insertionCost < minInsertionCost) {
                 minInsertionCost = insertionCost;
                 bestInsertPos = i + 1;
@@ -162,16 +171,16 @@ int *getTour_FarthestInsertion(double **dMatrix, int numOfCoords) {
         for (int i = tourLength; i >= bestInsertPos; --i) {
             tour[i] = tour[i - 1];
         }
-        tour[bestInsertPos] = farthestNode;
-        visited[farthestNode] = true;
+        tour[bestInsertPos] = globalFarthestNode;
+        visited[globalFarthestNode] = true;
+        numVisited++;
         tourLength++;
     }
 
-    // Add the start point at the end to complete the tour
-    tour[numOfCoords] = tour[0];
-
     // Clean up
     free(visited);
+    free(threadMaxCosts);
+    free(threadFarthestNodes);
 
     // Return the tour
     return tour;
