f680860202da8d8a3d1bfe14fc2a4db785bee69b
Ryan Liu <isryanyhliu@foxmail.com>
2024-01-07 04:25:15 +0000

    fix f

--- COMP528/Assignments/Assignment02/ompfInsertion.c
+++ COMP528/Assignments/Assignment02/ompfInsertion.c
@@ -18,7 +18,7 @@ double getDistance_FarthestInsertion(double **dMatrix, int numOfCoords, int poin
 
     int *tour = (int *)malloc((1 + numOfCoords) * sizeof(int));
     double totalDistance = 0.0;
-    
+
     bool *visited = (bool *)calloc(numOfCoords, sizeof(bool));
 
     for(int i = 0; i < numOfCoords; i++) {
@@ -131,62 +131,41 @@ int *getTour_FarthestInsertion(double **dMatrix, int numOfCoords, int pointOfSta
     int *nextNodes = (int *)malloc(numThreads * sizeof(int));
 
     while(numVisited < numOfCoords) {
+        double minCost = __DBL_MAX__;
         int bestNextNode = -1;
-        double globalMaxCost = 0;
+        int insertPos = -1;
 
-        #pragma omp parallel 
-        {
-            int threadID = omp_get_thread_num();
-            maxCosts[threadID] = 0;
-            nextNodes[threadID] = -1;
-
-            #pragma omp for nowait
-            for(int i = 0; i < tourLength - 1; i++) {
-                for(int j = 0; j < numOfCoords; j++) {
-                    if(!visited[j]) {
-                        double cost = dMatrix[tour[i]][j];
-                        if(cost > maxCosts[threadID]) {
-                            maxCosts[threadID] = cost;
-                            nextNodes[threadID] = j;
+        // OpenMP并行区域
+        #pragma omp parallel for reduction(min:minCost) private(j)
+        for (int j = 0; j < numOfCoords; j++) {
+            if (!visited[j]) {
+                for (int i = 0; i < tourLength - 1; i++) {
+                    double cost = dMatrix[tour[i]][j] + dMatrix[j][tour[i + 1]] - dMatrix[tour[i]][tour[i + 1]];
+                    if (cost < minCost) {
+                        #pragma omp critical
+                        {
+                            if (cost < minCost) {
+                                minCost = cost;
+                                bestNextNode = j;
+                                insertPos = i + 1;
+                            }
                         }
                     }
                 }
             }
+        }
 
-            // 在每个线程结束后执行，无需 critical
-            #pragma omp flush (globalMaxCost)
-            if (maxCosts[threadID] > globalMaxCost) {
-                #pragma omp critical
-                {
-                    if (maxCosts[threadID] > globalMaxCost) {
-                        globalMaxCost = maxCosts[threadID];
-                        bestNextNode = nextNodes[threadID];
-                    }
-                }
+        // 插入操作
+        if (bestNextNode != -1) {
+            for (int i = tourLength; i >= insertPos; i--) {
+                tour[i] = tour[i - 1];
             }
+            tour[insertPos] = bestNextNode;
+            visited[bestNextNode] = true;
+            totalDistance += minCost;
+            tourLength++;
+            numVisited++;
         }
-
-        double minCost = __DBL_MAX__;
-        int insertPos = -1;
-
-        // 不是并行的部分
-        for(int k = 0; k < tourLength - 1; k++) {
-            double cost = dMatrix[tour[k]][bestNextNode] + dMatrix[bestNextNode][tour[k + 1]] - dMatrix[tour[k]][tour[k + 1]];
-            if(cost < minCost - tolerance) {
-                minCost = cost;
-                insertPos = k + 1;
-            }
-        }
-
-        for(int i = numOfCoords; i > insertPos; i--) {
-            tour[i] = tour[i - 1];
-        }
-
-        tour[insertPos] = bestNextNode;
-        visited[bestNextNode] = true;
-        totalDistance += minCost;
-        tourLength++;
-        numVisited++;
     }
 
     free(maxCosts);
