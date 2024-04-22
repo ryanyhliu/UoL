e451fd268184df7f13ed7dc7bf108f567e942bd2
RyanLiu <isryanyhliu@foxmail.com>
2024-04-22 15:38:40 +0100

    AS02

--- COMP532/AS02/_HaihanSolution/train.py
+++ COMP532/AS02/_HaihanSolution/train.py
@@ -2,6 +2,7 @@ import gymnasium as gym
 from stable_baselines3 import PPO
 import torch
 import os
+import csv
 
 current_dir = os.path.dirname(__file__)
 models_path = os.path.join(current_dir, "models")
@@ -11,14 +12,7 @@ if not os.path.exists(models_path):
 env = gym.make('LunarLander-v2')
 env.reset()
 
-model = PPO("MlpPolicy", env, verbose=1)
-timesteps_per_iteration = 100
-iterations = 0
-total_timesteps = 0
-
-while True:
-    iterations += 1
-    model.learn(total_timesteps=timesteps_per_iteration, reset_num_timesteps=False)
-    if total_timesteps % 1000 == 0:
-        model.save(f"{models_path}/{total_timesteps}")
-        
+model = PPO("MlpPolicy", env, verbose=1, tensorboard_log=f"{current_dir}/tensorboard")
+# model = PPO("MlpPolicy", env, verbose=1)
+model.learn(total_timesteps=17000, reset_num_timesteps=False)
+model.save(f"{models_path}/ppo_lunar")
